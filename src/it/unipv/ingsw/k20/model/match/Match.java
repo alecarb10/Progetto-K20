b5e64706e3ceca0e91aca3bd7354ce9447665eb5
Your Name <josuarosales1798@gmail.com>
2020-04-29 21:53:29 +0200

    Aggiornato match

--- src/it/unipv/ingsw/k20/model/match/Match.java
+++ src/it/unipv/ingsw/k20/model/match/Match.java
@@ -1,57 +1,52 @@
 package it.unipv.ingsw.k20.model.match;
-import java.util.Date;
-import java.util.HashMap;
 import it.unipv.ingsw.k20.model.team.*;
+import java.util.Date;
 
 public class Match {
+	/*
 	private Date date;
 	private Team homeTeam;
 	private Team awayTeam;
 	private Stadium stadium;
 	private int homeScore;
 	private int awayScore;
-	
-	
+
 	public Match(Date date, Team homeTeam, Team awayTeam) {
 		this.date = date;
 		this.homeTeam = homeTeam;
 		this.awayTeam = awayTeam;
-		// this.stadium = homeTeam.getStadium(); manca la classe Stadium, e la classe Team con il suo stadio di casa
+		this.stadium = homeTeam.getStadium();
 		homeScore = 0;
 		awayScore = 0;
 	}
-	
-	public void setScore(int x, int y) {
-		
-	}
-	
-	/*
-	public Team getWinner() {
-	
-		if(homeTeam.getGoalsScored() > awayTeam.getGoalsScored()) {
-			homeTeam.setPoints(3);
-			return homeTeam;
-		} else if(homeTeam.getGoalsScored() < awayTeam.getGoalsScored()) {
-			awayTeam.setPoints(3);
-			return awayTeam;
-		}
-		else
-			homeTeam.setPoits(1);
-		    awayTeam.setPoits(1);
-			return null;
-	}
-	
-	public String toString() {
-		return "MATCH: " + homeTeam.getName() +"  VS  "+awayTeam.getName() + "    SCORE: " + homeTeam.getGoals() + "-" + awayTeam.getGoals() + 
-		"    DATE: " + date;    
-	}
-	*/
-	
-	
-	
-	
-	
-	
-	
 
+	public void setScore(int x, int y) {
+		homeScore = x;
+		homeTeam.setGoalsScored(homeTeam.getGoalsScored() + x);
+		homeTeam.setGoalsConceded(homeTeam.getGoalsConceded() + y);
+		awayScore = y;
+		awayTeam.setGoalsScored(awayTeam.getGoalsScored() + y);
+		awayTeam.setGoalsConceded(awayTeam.getGoalsConceded() + x);
+
+	}
+
+	public Team getWinner() {
+		if (homeScore > awayScore) {
+			homeTeam.setPoints(homeTeam.getPoints() + 3);
+			return homeTeam;
+		} else if (homeScore < awayScore) {
+			awayTeam.setPoints(awayTeam.getPoints() + 3);
+			return awayTeam;
+		} else
+			homeTeam.setPoints(homeTeam.getPoints() + 1);
+		awayTeam.setPoints(awayTeam.getPoints() + 1);
+
+		return null;
+	}
+
+	public String toString() {
+		return "MATCH: " + homeTeam.getName() + "  " + homeScore + "  VS  " + awayScore + "  " + awayTeam.getName()
+				+ "    DATE: " + date;
+	}
+*/
 }
