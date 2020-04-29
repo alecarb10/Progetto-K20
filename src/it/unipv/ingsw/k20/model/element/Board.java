60b17aa4e81b16769789c615abea43525489072e
danilomodica <danilo.modica01@universitadipavia.it>
2020-04-29 18:04:22 +0200

    Modificati elementi torneo + correzione package

--- src/it/unipv/ingsw/k20/model/element/Board.java
+++ src/it/unipv/ingsw/k20/model/element/Board.java
@@ -1,53 +1,35 @@
 package it.unipv.ingsw.k20.model.element;
 
-import java.util.Date;
 import java.util.List;
 
 import it.unipv.ingsw.k20.model.exception.OddTeamsSizeException;
 import it.unipv.ingsw.k20.model.team.Team;
+import it.unipv.ingsw.k20.model.util.ScheduleGenerator;
 
-public class Board {
-
-	private List<Team> teamsList;
-	private List<Day> schedule;
+public class Board extends TournamentElement {
 
 	public Board(List<Team> teamsList) {
-		this.teamsList = teamsList;
+		super(teamsList);
 	}
 
-	public void initBoard(int maxDays) throws OddTeamsSizeException {
+	@Override
+	public void initTournamentElement(int maxDays) throws OddTeamsSizeException {
 		schedule = ScheduleGenerator.getInstance().generateSchedule(teamsList, maxDays);
 		// ...
 	}
-
-	public List<Team> getTeamsList() {
-		return teamsList;
+	
+	@Override
+	public List<Team> getRanking() {
+		List<Team> ranking = teamsList;
+		
+		// prendo le squadre che si trovano nell'indice pari (chi passa il turno)
+		for (int i = 0; i < teamsList.size(); i++)
+			if (i % 2 == 0)
+				ranking.add(teamsList.get(i));
+		
+		return ranking;
 	}
-
-	public List<Day> getSchedule() {
-		return schedule;
-	}
-
-	public Day getDayByNumber(int number) {
-		Day day = null;
-
-		for (Day d : schedule)
-			if (d.getNumber() == number)
-				day = d;
-
-		return day;
-	}
-
-	public Day getDayByDate(Date date) {
-		Day day = null;
-
-		for (Day d : schedule)
-			if (d.getDate().equals(date))
-				day = d;
-
-		return day;
-	}
-
+	
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
--- src/it/unipv/ingsw/k20/model/element/Group.java
+++ src/it/unipv/ingsw/k20/model/element/Group.java
@@ -2,26 +2,25 @@ package it.unipv.ingsw.k20.model.element;
 
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 
 import it.unipv.ingsw.k20.model.exception.OddTeamsSizeException;
 import it.unipv.ingsw.k20.model.team.Team;
+import it.unipv.ingsw.k20.model.util.ScheduleGenerator;
 
-public class Group {
-
-	private List<Team> teamsList;
-	private List<Day> schedule;
+public class Group extends TournamentElement {
 
 	public Group(List<Team> teamsList) {
-		this.teamsList = teamsList;
+		super(teamsList);
 	}
 
-	public void initGroup(int maxDays) throws OddTeamsSizeException {
+	@Override
+	public void initTournamentElement(int maxDays) throws OddTeamsSizeException {
 		schedule = ScheduleGenerator.getInstance().generateSchedule(teamsList, maxDays);
 		// ...
 	}
 
+	@Override
 	public List<Team> getRanking() {
 		List<Team> ranking = teamsList;
 		
@@ -40,34 +39,6 @@ public class Group {
 		});
 		
 		return ranking;
-	}
-
-	public List<Team> getTeamsList() {
-		return teamsList;
-	}
-
-	public Day getDayByNumber(int number) {
-		Day day = null;
-		
-		for (Day d: schedule)
-			if (d.getNumber() == number)
-				day = d;
-		
-		return day;
-	}
-	
-	public Day getDayByDate(Date date) {
-		Day day = null;
-		
-		for (Day d: schedule)
-			if (d.getDate().equals(date))
-				day = d;
-		
-		return day;
-	}
-	
-	public List<Day> getSchedule() {
-		return schedule;
 	}
 
 	@Override
