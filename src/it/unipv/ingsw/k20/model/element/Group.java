a54e60df41302af903ccc7c4a0f0ed66d90cd22e
danilomodica <danilo.modica01@universitadipavia.it>
2020-05-02 17:04:19 +0200

    Completato Comparator della classifica

--- src/it/unipv/ingsw/k20/model/element/Group.java
+++ src/it/unipv/ingsw/k20/model/element/Group.java
@@ -10,7 +10,8 @@ import it.unipv.ingsw.k20.model.util.ScheduleGenerator;
 
 public class Group extends TournamentElement {
 
-	public Group() {}
+	public Group() {
+	}
 
 	@Override
 	public void initTournamentElement(int maxDays) throws OddTeamsSizeException {
@@ -21,24 +22,36 @@ public class Group extends TournamentElement {
 	@Override
 	public List<Team> getRanking() {
 		List<Team> ranking = teamsList;
-		
+
 		Collections.sort(ranking, new Comparator<Team>() {
 			@Override
 			public int compare(Team t1, Team t2) {
 
-				// if(t1.getPoints() > t2.getPoints())
-				// return -1;
-				//
-				// else if (t.getPoints() < t1.getPoints())
-				// return 1;
+				if (t1.getPoints() > t2.getPoints())
+					return -1;
 
-				return 0;
+				else if (t1.getPoints() < t2.getPoints())
+					return 1;
+
+				else {
+					int goalDifference1 = t1.getGoalsScored() - t1.getGoalsConceded();
+					int goalDifference2 = t2.getGoalsScored() - t2.getGoalsConceded();
+
+					if (goalDifference1 > goalDifference2)
+						return -1;
+
+					else if (goalDifference1 < goalDifference2)
+						return 1;
+
+					else
+						return 0;
+				}
 			}
 		});
-		
+
 		return ranking;
 	}
-	
+
 	@Override
 	public void endTournamentElement() {
 		completed = true;
@@ -49,7 +62,7 @@ public class Group extends TournamentElement {
 	public String toString() {
 		List<Team> ranking = getRanking();
 		StringBuilder sb = new StringBuilder();
-		
+
 		sb.append("Team Name" + "\t" + " Points" + "\n");
 		for (Team team : ranking) {
 			sb.append(team + "\n");
