0227dba64d2b436f0a59461cfa2278335131ad2c
frabariani <62015995+frabariani@users.noreply.github.com>
2020-05-02 09:58:41 +0200

    Aggiornata classe Manager

--- src/it/unipv/ingsw/k20/model/manager/Manager.java
+++ src/it/unipv/ingsw/k20/model/manager/Manager.java
@@ -5,78 +5,95 @@ import java.util.HashMap;
 import it.unipv.ingsw.k20.model.tournament.Tournament;
 
 public class Manager {
-	
-	private String id; //username
+
+	private String username;
 	private String name;
 	private String surname;
 	private String password;
+	private boolean isLogged;
 	private HashMap<Integer, Tournament> tournaments;
 
-	public Manager(String id, String name, String surname) {
-		this.id = id;
+	public Manager(String username, String name, String surname) {
+		this.username = username;
 		this.name = name;
 		this.surname = surname;
+		this.isLogged = false;
 		this.tournaments = new HashMap<>();
 	}
-	
-	public String getId() {
-		return id;
+
+	public String getUsername() {
+		return username;
 	}
-	
-	public void setId(String id) {
-		this.id = id;
+
+	public void setUsername(String id) {
+		this.username = username;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getSurname() {
 		return surname;
 	}
-	
+
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
-	
+
 	public HashMap<Integer, Tournament> getTournaments() {
 		return tournaments;
 	}
-	
-	/* Metodo che restituisce il Tournament relativo all'ID passato come parametro. */
+
+	/*
+	 * Metodo che restituisce il Tournament relativo all'ID passato come parametro.
+	 */
 	private Tournament getTournament(Integer tournamentID) {
-		if(this.tournaments.containsKey(tournamentID)) {
+		if (this.tournaments.containsKey(tournamentID)) {
 			return this.tournaments.get(tournamentID);
-		}
-		else return null;
+		} else
+			return null;
 	}
-	
+
 	/* Metodo che rimuove il Tournament relativo all'ID passato come parametro. */
 	private void removeTournament(Integer tournamentID) {
-		if(this.tournaments.containsKey(tournamentID)) {
+		if (this.tournaments.containsKey(tournamentID)) {
 			this.tournaments.remove(tournamentID);
 		}
 	}
-	
+
 	/* Metodo che aggiunge alla mappa il Tournament passato come parametro. */
 	public void addTournament(Tournament tournament) throws NullPointerException {
 		this.tournaments.put(tournament.getTournamentID(), tournament);
 	}
 
-	@Override
-	public String toString() {
-		return "Manager ID: " + id + "\n\tName: " + name + "\n\tSurname: " + surname;
+	public boolean isLoggedIn() {
+		return this.isLogged;
 	}
 
-	public boolean register() { 
+	/* Metodo che permette di modificare la password. */
+	public boolean changePassword(String oldPwd, String newPwd) {
+		if (this.password == oldPwd) {
+			this.password = newPwd;
+			return true;
+		} else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Manager Username: " + username + "\n\tName: " + name + "\n\tSurname: " + surname;
+	}
+
+	public boolean register() {
 		return false;
 	}
-	
-	public boolean loign() { 
+
+	public boolean loign() {
 		return false;
-	} 
+	}
 }
