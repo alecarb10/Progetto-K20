be06318031e0a982f56a47dbffc848dce47b1a54
Your Name <josuarosales1798@gmail.com>
2020-05-05 23:52:03 +0200

    vista del fan

---
+++ src/it/unipv/ingsw/k20/view/gui/fan/controller/controllerFan.java
@@ -0,0 +1,5 @@
+package it.unipv.ingsw.k20.view.gui.fan.controller;
+
+public class controllerFan {
+
+}
---
+++ src/it/unipv/ingsw/k20/view/gui/fan/main/mainFan.java
@@ -0,0 +1,28 @@
+package it.unipv.ingsw.k20.view.gui.fan.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class mainFan extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader root = new FXMLLoader(
+				getClass().getResource("/it/unipv/ingsw/k20/view/gui//fan/resources/viewFan.fxml"));
+		Scene scene = new Scene(root.load());
+		scene.getStylesheets()
+				.add(getClass().getResource("/it/unipv/ingsw/k20/view/gui//fan/style/styleFan.css").toExternalForm());
+		primaryStage.setTitle("user");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+
+	}
+
+	public static void main(String args[]) {
+		launch(args);
+	}
+
+}
