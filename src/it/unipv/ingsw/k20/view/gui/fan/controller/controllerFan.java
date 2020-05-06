00362294aa009ee71e05f52be9feea6469a5c9b6
Your Name <josuarosales1798@gmail.com>
2020-05-06 12:44:02 +0200

    fan view aggiornato

--- src/it/unipv/ingsw/k20/view/gui/fan/controller/controllerFan.java
+++ src/it/unipv/ingsw/k20/view/gui/fan/controller/controllerFan.java
@@ -1,5 +1,16 @@
 package it.unipv.ingsw.k20.view.gui.fan.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 public class controllerFan {
+	@FXML
+	private ListView tournamentList;
+	
+	public controllerFan() {
+		this.tournamentList = new ListView();
+	}
+	
+	
 
 }
--- src/it/unipv/ingsw/k20/view/gui/fan/main/mainFan.java
+++ src/it/unipv/ingsw/k20/view/gui/fan/main/mainFan.java
@@ -1,5 +1,6 @@
 package it.unipv.ingsw.k20.view.gui.fan.main;
 
+import it.unipv.ingsw.k20.view.gui.fan.controller.controllerFan;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,6 +12,7 @@ public class mainFan extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		FXMLLoader root = new FXMLLoader(
 				getClass().getResource("/it/unipv/ingsw/k20/view/gui//fan/resources/viewFan.fxml"));
+		root.setController(new controllerFan());
 		Scene scene = new Scene(root.load());
 		scene.getStylesheets()
 				.add(getClass().getResource("/it/unipv/ingsw/k20/view/gui//fan/style/styleFan.css").toExternalForm());
