6c4d9d0d268352083bfad50dc37e6f9496e92163
hmnsin <haman.singh01@universitadipavia.it>
2020-05-24 22:10:22 +0200

    modifica sezione edit team (gui)

--- src/mvc/view/manager/gui/controller/EditTeamController.java
+++ src/mvc/view/manager/gui/controller/EditTeamController.java
@@ -2,20 +2,21 @@ package mvc.view.manager.gui.controller;
 
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 import database.dao.impl.FacadeImpl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import mvc.model.team.Stadium;
 import mvc.model.team.Team;
 import mvc.model.tournament.Tournament;
+import mvc.view.manager.gui.util.GraphicControlsHandler;
 
 public class EditTeamController implements Initializable {
 
@@ -24,19 +25,37 @@ public class EditTeamController implements Initializable {
 	private ComboBox<String> cmbBoxTournament,cmbBoxTeam;
 	
 	@FXML
-	private TextField txtFldTeamName,txtFldStadiumName,txtFldStadiumCity,txtFldStadiumCapacity;
+	private TextField txtFldTeamName,txtFldStadiumName,txtFldStadiumCity,txtFldStadiumCapacity,txtFldTeamCoach;
+	@FXML
+	private Button btnSave;
 	
 	private String username;
-	private ObservableList<String> tournaments;	
+	private ObservableList<String> tournaments,teams;	
 	private FacadeImpl facadeImpl;
 	private List<Tournament> tournamentsList;
+	private List<Team> teamsList;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		tournaments= FXCollections.observableArrayList();
 		facadeImpl= new FacadeImpl();
 		this.cmbBoxTournament.setOnAction((ActionEvent)->{
-			this.cmbBoxTeam.setItems(this.getTeamsList());
+			teams=getTeamsList();
+			if(teams !=null)
+				this.cmbBoxTeam.setItems(teams);
+		});
+		this.cmbBoxTeam.setOnAction((ActionEvent)->{
+			Team team= getTeam(cmbBoxTeam.getSelectionModel().getSelectedItem());
+			Stadium stadium=null;
+			if(team!=null) {
+				txtFldTeamName.setText(team.getName());
+				stadium=team.getStadium();	
+			}
+			if(stadium!=null) {				
+				txtFldStadiumName.setText(stadium.getName());
+				txtFldStadiumCity.setText(stadium.getCity());
+				txtFldStadiumCapacity.setText(Integer.toString(stadium.getCapacity()));
+			}
 		});
 
 	}
@@ -45,7 +64,7 @@ public class EditTeamController implements Initializable {
 		this.username=username;
 	}
 	
-	public void setTournamentsMap(List<Tournament> tournamentsList) {
+	public void setTournamentsList(List<Tournament> tournamentsList) {
 		this.tournamentsList=tournamentsList;
 	}
 	
@@ -57,7 +76,8 @@ public class EditTeamController implements Initializable {
 	private ObservableList<String> getTeamsList(){
 		ObservableList<String> teams=FXCollections.observableArrayList();
 		try {
-			List<Team> teamsList=facadeImpl.getTeamsByTournament(getTournament(cmbBoxTournament.getSelectionModel().getSelectedItem()));
+			if(cmbBoxTournament.getValue()!=null)
+				teamsList=facadeImpl.getTeamsByTournament(getTournament(cmbBoxTournament.getSelectionModel().getSelectedItem()));
 			for(Team t: teamsList) 
 				teams.add(t.getName());
 		} catch (Exception ex) {
@@ -72,5 +92,36 @@ public class EditTeamController implements Initializable {
 				return tournament;
 		}
 		return null;
+	}
+	
+	private Team getTeam(String name) {
+		for(Team team:teamsList)
+			if(team.getName().equals(name))
+				return team;
+		return null;
+		
+	}
+	
+	public void saveTeam(ActionEvent event) {
+		try {
+			Stadium stadium= new Stadium(txtFldStadiumName.getText(), txtFldStadiumCity.getText(),Integer.parseInt(txtFldStadiumCapacity.getText()));
+			facadeImpl.storeStadium(stadium);
+			restoreComponents();
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+	
+	private void restoreComponents() {
+		GraphicControlsHandler.resetComboBox(this.cmbBoxTournament, "Select Tournament");
+		GraphicControlsHandler.resetComboBox(this.cmbBoxTeam, "Select Team");
+		GraphicControlsHandler.resetTextField(this.txtFldTeamName, "Name");
+		GraphicControlsHandler.resetTextField(this.txtFldTeamCoach, "Coach");
+		GraphicControlsHandler.resetTextField(this.txtFldStadiumName, "Name");
+		GraphicControlsHandler.resetTextField(this.txtFldStadiumCity, "City");
+		GraphicControlsHandler.resetTextField(this.txtFldStadiumCapacity, "Capacity");
+		GraphicControlsHandler.resetObservableList(this.teams);
 	}
 }
