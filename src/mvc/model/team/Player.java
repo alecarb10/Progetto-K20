cebd412b6f6b9b12accd4d8fecb74193e463e44f
hmnsin <haman.singh01@universitadipavia.it>
2020-06-03 20:46:57 +0200

    aggiunti setName() e setSurname()

--- src/mvc/model/team/Player.java
+++ src/mvc/model/team/Player.java
@@ -35,8 +35,16 @@ public class Player {
 		return this.name;
 	}
 	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
 	public String getSurname() {
 		return surname;
+	}
+	
+	public void setSurname(String surname) {
+		this.surname=surname;
 	}
 
 	public void setNumber(int number) {
