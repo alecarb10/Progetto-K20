1a06d0ca2687ef953593b44514e819edaa1f0629
Your Name <josuarosales1798@gmail.com>
2020-06-24 19:07:23 +0200

    knockoutPhase8 provvisorio

--- src/clientserver/client/fan/gui/view/knockoutphase/KnockoutPhase8Controller.java
+++ src/clientserver/client/fan/gui/view/knockoutphase/KnockoutPhase8Controller.java
@@ -1,18 +1,140 @@
 package clientserver.client.fan.gui.view.knockoutphase;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
+import database.dao.impl.FacadeImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import mvc.model.element.Day;
+import mvc.model.match.Match;
+import mvc.model.tournament.KnockoutPhase;
 
-public class knockoutphase8Controller {
+public class KnockoutPhase8Controller implements Initializable {
 	@FXML
 	Button backButton;
+	@FXML
+	Text textBoard;
+	@FXML
+	Label label1;
+	@FXML
+	Label label2;
+	@FXML
+	Label label3;
+	@FXML
+	Label label4;
+	@FXML
+	Label label5;
+	@FXML
+	Label label6;
+	@FXML
+	Label label7;
+	@FXML
+	Label label8;
+	@FXML
+	Label label9;
+	@FXML
+	Label label10;
+	@FXML
+	Label label11;
+	@FXML
+	Label label12;
+	@FXML
+	Label label13;
+	@FXML
+	Label label14;
+	@FXML
+	Label label15;
+	
+
+	List<Label> labelDay1c = new ArrayList<>();
+	List<Label> labelDay1f = new ArrayList<>();	
+	List<Label> labelDay2c = new ArrayList<>();
+	List<Label> labelDay2f = new ArrayList<>();
+	List<Label> labelDay3c = new ArrayList<>();
+	List<Label> labelDay3f = new ArrayList<>();	
+	
+	FacadeImpl facade = FacadeImpl.getInstance();
+	List<Day> days;
+	
+	public void passingDataToKnock8(KnockoutPhase k8) throws SQLException {
+		textBoard.setText(k8.getName());
+		days = facade.getSchedule(k8,false);
+		
+		
+		for(Day day : days) {
+			for(Match match : day.getMatchesList()) {
+				System.out.println(match.toString());
+			}
+		}
+		
+		
+		label1.setText(days.get(0).getMatchesList().get(0).getHomeTeam().getName() +"            "+ days.get(0).getMatchesList().get(0).getHomeScore());
+		label2.setText(days.get(0).getMatchesList().get(0).getAwayTeam().getName() +"            " +days.get(0).getMatchesList().get(0).getAwayScore());
+		
+		label3.setText(days.get(0).getMatchesList().get(1).getHomeTeam().getName() +"            "+ days.get(0).getMatchesList().get(1).getHomeScore());
+		label4.setText(days.get(0).getMatchesList().get(1).getAwayTeam().getName() +"            " +days.get(0).getMatchesList().get(1).getAwayScore());
+		
+		label5.setText(days.get(0).getMatchesList().get(2).getHomeTeam().getName() +"            "+ days.get(0).getMatchesList().get(2).getHomeScore());
+		label6.setText(days.get(0).getMatchesList().get(2).getAwayTeam().getName() +"            " +days.get(0).getMatchesList().get(2).getAwayScore());
+		
+		label7.setText(days.get(0).getMatchesList().get(3).getHomeTeam().getName() +"            "+ days.get(0).getMatchesList().get(3).getHomeScore());
+		label8.setText(days.get(0).getMatchesList().get(3).getAwayTeam().getName() +"            " +days.get(0).getMatchesList().get(3).getAwayScore());
+		
+
+		label9.setText(days.get(1).getMatchesList().get(0).getHomeTeam().getName() +"            "+ days.get(1).getMatchesList().get(0).getHomeScore());
+		label10.setText(days.get(1).getMatchesList().get(0).getAwayTeam().getName() +"            " +days.get(1).getMatchesList().get(0).getAwayScore());
+
+		label11.setText(days.get(1).getMatchesList().get(1).getHomeTeam().getName() +"            "+ days.get(1).getMatchesList().get(1).getHomeScore());
+		label12.setText(days.get(1).getMatchesList().get(1).getAwayTeam().getName() +"            " +days.get(1).getMatchesList().get(1).getAwayScore());
+		
+
+		label13.setText(days.get(2).getMatchesList().get(0).getHomeTeam().getName() +"            "+ days.get(2).getMatchesList().get(0).getHomeScore());
+		label14.setText(days.get(2).getMatchesList().get(0).getAwayTeam().getName() +"            " +days.get(2).getMatchesList().get(0).getAwayScore());
+		
+		label15.setText(days.get(2).getMatchesList().get(0).getWinner().getName());
+		
+		/*
+		labelDay1c.add(label1);
+		labelDay1f.add(label2);
+		labelDay1c.add(label3);
+		labelDay1f.add(label4);
+		labelDay1c.add(label5);
+		labelDay1f.add(label6);
+		labelDay1c.add(label7);
+		labelDay1f.add(label8);
+		
+		labelDay2c.add(label9);
+		labelDay2f.add(label10);
+		labelDay2c.add(label11);
+		labelDay2f.add(label12);
+		
+		labelDay3c.add(label13);
+		labelDay3f.add(label14);
+		
+		*/
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 	
 	public void backButtonClicked(ActionEvent event) throws IOException {
 		FXMLLoader loader = new FXMLLoader();
@@ -22,6 +144,18 @@ public class knockoutphase8Controller {
 		primaryStage.setTitle("Fan menu");
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+	}
+
+
+
+
+
+
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
 		
 	}
 
