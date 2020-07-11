57bc97d8b3479e872904519f378657d4ccc53b45
danilomodica <danilo.modica01@universitadipavia.it>
2020-07-11 21:40:50 +0200

    Implementata home (test) + aggiunta sezione dettagli torneo(test)

--- src/interfaces/fan/webapp/servlet/HomeServlet.java
+++ src/interfaces/fan/webapp/servlet/HomeServlet.java
@@ -1,6 +1,8 @@
 package interfaces.fan.webapp.servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -8,21 +10,45 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.rythmengine.Rythm;
 
+import services.persistence.dao.impl.FacadeImpl;
+
+import model.tournament.Tournament;
+
 @SuppressWarnings("serial")
 public class HomeServlet extends WebServlet {
 
+	private List<Tournament> tournaments;
+	
 	public HomeServlet(String name, String url) {
 		super(name, url);
 	}
 	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		if (req.getPathInfo().equals("/") || req.getPathInfo().equals("/home.html"))
-			resp.getWriter().write(Rythm.render("home.html"));
+		
+		if (req.getPathInfo().equals("/") || req.getPathInfo().equals("/home.html")) {
+			try {
+				tournaments = FacadeImpl.getInstance().getAllTournaments();
+				resp.getWriter().write(Rythm.render("home.html", tournaments));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		resp.sendRedirect("/");
+		if (req.getPathInfo().equals("/view")) {
+			Tournament tournament = null;;
+			for (Tournament t: tournaments)
+				if (t.getId() == Integer.parseInt(req.getParameter("id")))
+					tournament = t;
+			try {
+				tournament.addTeams(FacadeImpl.getInstance().getTeamsByTournament(tournament));
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			resp.getWriter().write(Rythm.render("tournamentdetail.html", tournament));	
+		} 
 	}
 }
