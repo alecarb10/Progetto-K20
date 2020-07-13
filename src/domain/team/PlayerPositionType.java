520f40adc2b73a5e88b55c877dd3fea998e8f4ce
frabariani <62015995+frabariani@users.noreply.github.com>
2020-07-13 19:48:28 +0200

    Javadoc Team

--- src/domain/team/PlayerPositionType.java
+++ src/domain/team/PlayerPositionType.java
@@ -1,5 +1,13 @@
 package domain.team;
 
+/**
+ * This enum represents player's position type:
+ * MF = Mid Fielder
+ * CB = Centre Back
+ * CF = Central Forward
+ * GK = Goal Keeper 
+ * 
+ */
 public enum PlayerPositionType {
 	GK,CB,MF,CF;
 }
