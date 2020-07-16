f21ade422c7089e58577450abac3aaa98a65228d
hmnsin <haman.singh01@universitadipavia.it>
2020-07-16 12:12:40 +0200

    manager gui: aggiunta javadoc alla classe Main

--- src/interfaces/manager/gui/Main.java
+++ src/interfaces/manager/gui/Main.java
@@ -6,6 +6,10 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Class to run the manager gui
+ */
+
 public class Main extends Application {
 
 	@Override
