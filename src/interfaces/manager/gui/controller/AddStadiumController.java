3807448e7bfeec5e5435b35611c2d4cb4b5b1a68
hmnsin <haman.singh01@universitadipavia.it>
2020-07-16 23:27:25 +0200

    add stadium controller: aggiunta controlli + alert

--- src/interfaces/manager/gui/controller/AddStadiumController.java
+++ src/interfaces/manager/gui/controller/AddStadiumController.java
@@ -11,52 +11,56 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import services.persistence.dao.impl.FacadeImpl;
 
 /**
  * Controller for add the stadium
+ * 
  * @see Initializable
  * @see Stadium
  */
 
 public class AddStadiumController implements Initializable {
-	
+
 //	@FXML
 //	private ComboBox<String> cmbBoxTournament;
 //	
 //	private String username;
 //	private ObservableList<String> tournaments = FXCollections.observableArrayList();
 	@FXML
-	private TextField txtFldStadiumName,txtFldStadiumCity,txtFldStadiumCapacity;
+	private TextField txtFldStadiumName, txtFldStadiumCity, txtFldStadiumCapacity;
 	@FXML
 	private Button btnAddStadium;
 	@FXML
 	private TableView<Stadium> tblViewStadium;
 	@FXML
-	private TableColumn<Stadium, String> tblClmnStadiumName,tblClmnStadiumCity,tblClmnStadiumCapacity;
-	
+	private TableColumn<Stadium, String> tblClmnStadiumName, tblClmnStadiumCity, tblClmnStadiumCapacity;
+
 	private ObservableList<Stadium> stadiums;
 	private FacadeImpl facadeImpl;
 	private List<Stadium> stadiumsList;
-	
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		facadeImpl= FacadeImpl.getInstance();
-		stadiums=FXCollections.observableArrayList();
-		stadiumsList=getStadiums();
-		for(Stadium s:stadiumsList)
+		facadeImpl = FacadeImpl.getInstance();
+		stadiums = FXCollections.observableArrayList();
+		stadiumsList = getStadiums();
+		for (Stadium s : stadiumsList)
 			stadiums.add(s);
 		tblClmnStadiumName.setCellValueFactory(new PropertyValueFactory<Stadium, String>("Name"));
 		tblClmnStadiumCity.setCellValueFactory(new PropertyValueFactory<Stadium, String>("City"));
 		tblClmnStadiumCapacity.setCellValueFactory(new PropertyValueFactory<Stadium, String>("Capacity"));
 		tblViewStadium.setItems(stadiums);
 	}
-	
+
 	private List<Stadium> getStadiums() {
 		try {
 			return facadeImpl.getStadiums();
@@ -65,22 +69,34 @@ public class AddStadiumController implements Initializable {
 			return null;
 		}
 	}
-	
+
 	public void addStadium(ActionEvent event) {
 		try {
-			Stadium stadium= new Stadium(txtFldStadiumName.getText(), txtFldStadiumCity.getText(),Integer.parseInt(txtFldStadiumCapacity.getText()));
-			stadiumsList.add(stadium);
-			stadiums.add(stadium);
-			facadeImpl.storeStadium(stadium);
-			restoreComponents();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			if (isNotBlankControl()) {
+				Stadium stadium = new Stadium(txtFldStadiumName.getText(), txtFldStadiumCity.getText(), Integer.parseInt(txtFldStadiumCapacity.getText()));
+				stadiumsList.add(stadium);
+				stadiums.add(stadium);
+				facadeImpl.storeStadium(stadium);
+				Alert alert=new Alert(AlertType.INFORMATION,"Stadium successfully created.",ButtonType.OK);
+				alert.showAndWait();
+				if(alert.getResult()==ButtonType.OK) 
+					restoreComponents();
+			} else
+				new Alert(AlertType.ERROR, "Fields cannot be empty.", ButtonType.OK).show();
+		} catch (NumberFormatException nfe) {
+			new Alert(AlertType.ERROR, "Stadium capacity must be a number.", ButtonType.OK).show();
+		} catch (Exception e) {
+			new Alert(AlertType.ERROR, e.getMessage(), ButtonType.OK).show();
 		}
 	}
-	
+
 	private void restoreComponents() {
 		GraphicControlsHandler.resetTextField(this.txtFldStadiumName, "Name");
 		GraphicControlsHandler.resetTextField(this.txtFldStadiumCity, "City");
 		GraphicControlsHandler.resetTextField(this.txtFldStadiumCapacity, "Capacity");
+	}
+
+	private boolean isNotBlankControl() {
+		return !txtFldStadiumName.getText().isBlank() && !txtFldStadiumCity.getText().isBlank() && !txtFldStadiumCapacity.getText().isBlank();
 	}
 }
